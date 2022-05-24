7539f21b3f1149229761c055156032a540a4771e
vijaykumar <vijay1111mane@gmail.com>
2022-05-24 19:01:12 +0530

    commite;

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,40 @@
   <div id="app">
     <h1>Props in Vue js</h1>
     <Child v-bind:users="users" />
+    <br>
+    <h1>{{title}}</h1>
+    <Child_parent v-on:changeTitle="updateTitle($event)" />
   </div>
 </template>
 
 <script>
 import Child from "./components/Child";
-
+import Child_parent from './components/Child_parent';
 export default {
   name: "App",
   components: {
     Child,
+    Child_parent
   },
+  // data(){
+  //  return {users:[
+  //    {name:"Vijay", email:"vijay@gmail.com"},
+  //    {name:"Gokul",email:"gokul@gmail.com"},
+  //    {name:"Nithin",email:"nithin@gmail.com"},
+  //    {name:"Prajwal",email:"prajwal@gmail.com"}
+  //    ]}
+  // },
+
+  // Child to parent (Child_parent)
   data(){
-   return {users:[
-     {name:"Vijay", email:"vijay@gmail.com"},
-     {name:"Gokul",email:"gokul@gmail.com"},
-     {name:"Nithin",email:"nithin@gmail.com"},
-     {name:"Prajwal",email:"prajwal@gmail.com"}
-     ]}
+     return {title:"Props tutorial"}
+  },
+  methods:{
+    updateTitle(title){
+      this.title=title
+    }
   }
+
 };
 </script>
 
---
+++ src/components/Child_parent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <button v-on:click="updateTitle">Update Parent Title</button>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'Child_parent',
+    methods:{
+        updateTitle(){
+            this.$emit('changeTitle','Parent Updated')
+        }
+    }
+
+}
+</script>
